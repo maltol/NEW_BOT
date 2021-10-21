237098436147bb9e5b24313cfc96df309d7aff27
Yevhenii <jenyamazka@gmail.com>
2021-10-21 13:38:39 +0300

    test 1.0.2

--- funcs_parsing.py
+++ funcs_parsing.py
@@ -12,18 +12,17 @@ def short_report():
     return ('\n'.join(error_array))
 
 def parsing1():
-    df = pd.read_excel('lands.xlsx')
-    pd.set_option('display.max_rows', None)
+    write_doc = open("lands_list.txt")
     array_normal = []
     array_errors = []
-    for index, websites in df.iterrows():
+    for line in write_doc:
+        line = line.rstrip('\n')
         try:
-            response = requests.get(websites["Name"])
-            df.at[index, 'status_code'] = "{}".format(response.status_code)
+            response = requests.get(line)
             if response.status_code == 200:
-                array_normal.append("\U00002705""{}".format(response.status_code) + ' ' + websites["Name"])
+                array_normal.append("\U00002705" + str(response.status_code) + ' ' + line)
             elif response.status_code > 200:
-                array_errors.append("\U0000274C""{}".format(response.status_code) + ' ' + websites["Name"])
+                array_errors.append("\U0000274C" + str(response.status_code) + ' ' + line)
         except requests.ConnectionError:
             pass
     return ("\n".join(array_normal + array_errors))
